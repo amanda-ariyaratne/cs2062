95ac8201543a5da2e4acce0362358a2a063e3fc6
Amanda Ariyaratne <aariyaratne@gmail.com>
2019-02-27 20:04:12 +0530

    MVC structure initial push

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+ 	
+	define('DEBUG', true);
+
+ 	define('DEFAULT_CONTROLLER', 'Home'); // default controller
+	define('DEFAULT_LAYOUT', 'default'); //default layout
+
+	define('PROOT', '/cs2062/'); //set this to '/' for a live server
+
+	define('SITE_TITLE', 'CS 2062');
