ea0e5bfb11de13124d8eb6db2f65fd94df8f5c5b
chamodi <madhushanichamodi@gmail.com>
2019-03-05 06:29:47 +0530

    Adding categories, images and styling

---
+++ app/views/home/Categories.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="assets/css/bootstrap.4x.css">
+    <link rel="stylesheet" href="assets/css/Categories.css">
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="categories">
+    <h4 class="categories-title">Categories</h4>
+    <ul class="categories-list">
+
+        <li class="cat-item cat-item-70 cat-parent current-cat-parent">
+            <span class="list-icon">
+                <img src="assets/images/categories_icons/791499-200.png">
+            </span>
+            <a href="#" class="line">Clothes</a>
+            <!--<span class="count">(12)</span>-->
+            <a data-toggle="collapse" class="collapsed" href="#collapse1"><i class="far"></i></a>
+            <div id="collapse1" class="panel-collapse collapse">
+                <ul id="children-of-70" class="children collapse in">
+                    <li>
+                        <span class="list-icon">
+                            <img src="assets/images/categories_icons/Business_formal_shirt-512.png"
+                                 >
+                        </span>
+                        <a href="#">Men</a>
+                        <!--<span class="count">(2)</span>-->
+                    </li>
+
+                    <li>
+                        <span class="list-icon">
+                            <img 
+                            src="assets/images/categories_icons/female_clothes-05-512.png">
+                        </span>
+                        <a href="#">Women</a>                        
+                    </li>
+
+                    <li>
+                    	<span class="list-icon">
+                            <img 
+                                 src="assets/images/categories_icons/32-512.png">
+                             </span>
+                        <a href="#" class="line">Kids & Babies</a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+
+        <li class="cat-item cat-item-80 cat-parent current-cat-parent">
+            <span class="list-icon">
+                <img src="assets/images/categories_icons/Home-Decorations.png">
+            </span>
+            <a href="#" class="line">Home Decor</a>
+            <!--<span class="count">(12)</span>-->
+            <a data-toggle="collapse" class="collapsed" href="#collapse2"><i class="far"></i></a>
+            <div id="collapse2" class="panel-collapse collapse">
+                <ul id="children-of-80" class="children collapse in">
+                    <li>
+                        <span class="list-icon">
+                            <img src="assets/images/categories_icons/140090-200.png">
+                        </span>
+                        <a href="#">Curtains</a>
+                        <!--<span class="count">(2)</span>-->
+                    </li>
+
+                    <li>
+                        <span class="list-icon">
+                            <img src="assets/images/categories_icons/il_570xN.1706629259_stie.jpg">
+                        </span>
+                        <a href="#">Cushion Covers</a>                        
+                    </li>
+
+                    <li>
+                        <span class="list-icon">
+                            <img src="assets/images/categories_icons/bedroom-Line-02-512.png">
+                        </span>
+                        <a href="#">Pillow Covers</a>
+                    </li>
+
+                    <li>
+                        <span class="list-icon">
+                            <img src="assets/images/categories_icons/bed_355610.png" >
+                        </span>
+                        <a href="#">Bedsheets</a>
+                    </li>
+
+                    <li>
+                        <span class="list-icon">
+                            <img src="assets/images/categories_icons/181443727Untitled-3-512.png">
+                        </span>
+                        <a href="#" class="line">Wall Arts</a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+
+        <li>
+            <span class="list-icon">
+                <img src="assets/images/categories_icons/Very-Basic-Briefcase-icon.png">
+            </span>
+            <a href="#" class="line">Device Cases</a>
+        </li>
+
+        <li>
+            <span class="list-icon">
+                <img src="assets/images/categories_icons/62d8e49f120d2cb54a3b0a50396d3f30.jpg">
+            </span>
+            <a href="#" class="line">Bags</a>
+        </li>
+
+        <li class="cat-item cat-item-90 cat-parent current-cat-parent">
+            <span class="list-icon">
+                <img src="assets/images/categories_icons/117792-200.png">
+            </span>
+            <a href="#" class="line">Other</a>
+            <!--<span class="count">(12)</span>-->
+            <a data-toggle="collapse" class="collapsed" href="#collapse3"><i class="far"></i></a>
+            <div id="collapse3" class="panel-collapse collapse">
+                <ul id="children-of-90" class="children collapse in">
+                    <li>
+                        <span class="list-icon">
+                            <img src="assets/images/categories_icons/97-512.png">
+                        </span>
+                        <a href="#">Greeting Cards</a>
+                        <!--<span class="count">(2)</span>-->
+                    </li>
+
+                    <li>
+                        <span class="list-icon">
+                            <img src="assets/images/categories_icons/clothes_030-earring-accessory-fashion-jewelry-512.png">
+                        </span>
+                        <a href="#">Handmade Jewellery</a>                        
+                    </li>
+
+                    <li>
+                        <span class="list-icon">
+                            <img src="assets/images/categories_icons/teddy-bear-clip-art-39.png">
+                        </span>
+                        <a href="#" >Toys</a>
+                    </li>    
+                </ul>
+            </div>
+        </li>
+    </ul>
+</div>
+</body>
+</html>
