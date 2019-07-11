c5afd456bae64e9a349b8cf6a1875c406ed8bb38
MadhaviG97 <madhavi.17@cse.mrt.ac.lk>
2019-07-11 14:50:00 +0530

    check 1

--- app/controllers/Account.php
+++ app/controllers/Account.php
@@ -39,7 +39,6 @@
 
 				    $user = new User();
 				    if ($user->findByEmail($email) == null){
-				    	
 				    	if ($role == 2) {
 				    		$role = 4;
 				    	}
@@ -75,13 +74,7 @@
 				    	//user->login();
 				    } else {
 				    	$_SESSION['error_email'] = "<div style='color: red;'>This user already exists</div>";
-				    	if ($role == 1) {
-				    		$this->view->render('account/adminRegister');
-				    	} else if($role == 2){
-				    		$this->view->render('account/tailorRegister1');
-				    	} else {
-				    		$this->view->render('account/customerRegister1');
-				    	}
+				    	$this->view->render('account/register');
 				    }
 				
 			} else {
@@ -93,10 +86,6 @@
 				$this->view->render('account/register');
 			}
 			
-		}
-
-		public function adminRegister1Action(){
-			$this->view->render('account/adminRegister');
 		}
 
 		public function loginAction(){
